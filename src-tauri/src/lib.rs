b9e821c0f764ef1e3014d5509f8f45210db5e18f
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2026-05-07 09:47:38 -0300

    fix: register tauri-plugin-dialog in builder to enable file picker

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init());
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init());
 
     #[cfg(debug_assertions)]
     {
